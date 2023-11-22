2e105cfff2da47bf63e42fd6ba11efa4e3974303
Kemilyy <133906744+Kemilyy@users.noreply.github.com>
2023-11-22 16:18:05 -0300

    Tentativa de login falha ebaaa

--- js/login.js
+++ js/login.js
@@ -20,3 +20,33 @@ window.onclick = function (event) {
         switchModal();
     }
 }
+
+
+function validarLogin() {
+    // Verifique se os campos de email e senha estão preenchidos.
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("email").value;
+  
+    if (email === "" || senha === "") {
+      alert("Preencha todos os campos.");
+      return false;
+    }
+  
+    // Verifique se o email é válido.
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regexEmail.test(email)) {
+      alert("O email é inválido.");
+      return false;
+    }
+  
+    // Verifique se a senha é válida.
+    const regexSenha = /^(*[0-9])$/;
+    if (!regexSenha.test(senha)) {
+      alert("A senha deve ter pelo menos 8 caracteres, incluindo números, letras maiúsculas e minúsculas.");
+      return false;
+    }
+  
+    window.location.href = "./index.html"
+    return true;
+  }
+  
